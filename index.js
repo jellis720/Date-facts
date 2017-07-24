effab6a66f5e023bfee610a7d06afe468b6d1c58
jellis720 <jellis720@gmail.com>
2017-07-24 16:46:18 -0400

    final commit

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const chalk = require('chalk');
 const moment = require('moment');
+// const isDST = moment().isDST()
 
-console.log(chalk.green(''));
+// console.log(isDST);
+function dst() {
+  if (moment().isDST() == true) {
+    return "is"
+  } else {
+    return "is not"
+  }
+  return dst
+}
 
-const moment = require('moment');
+function lpy() {
+  if (moment().isLeapYear() == true) {
+    return "is"
+  } else {
+    return "is not"
+  }
+  return lpy
+}
 
-console.log(moment())
+// console.log(chalk.green(''));
+console.log("It is " + chalk.blue(moment().format("dddd, MMMM, Do YYYY, h:mm:ss a")));
+
+console.log("It is the " + chalk.magenta(moment().format("DDDo")) + " day of the year");
+
+console.log("It is " + chalk.cyan(`${moment().hours()*60*60 + moment().minutes()*60 + moment().seconds()}`) + " seconds into the day");
+
+console.log("It " + chalk.green(dst()) + " Daylight Savings Time.");
+
+console.log("It " + chalk.red(lpy()) + " a leap year.");
